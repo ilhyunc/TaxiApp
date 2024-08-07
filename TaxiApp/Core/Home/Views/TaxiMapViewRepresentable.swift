13b48dcd41b3fface3bba2f4ebcdd8bb29c80c45
Ilhyun Cho <icho8@myseneca.ca>
2024-08-07 11:28:07 -0400

    Aug - 7

---
+++ TaxiApp/Core/Home/Views/TaxiMapViewRepresentable.swift
@@ -0,0 +1,116 @@
+//
+//  TaxiMapViewREpresentable.swift
+//  TaxiApp
+//
+//  Created by Joe on 2024-07-23.
+//
+
+import SwiftUI
+import MapKit
+
+struct TaxiMapViewRepresentable: UIViewRepresentable{
+    
+    let mapView = MKMapView()
+    //let locationManager = LocationManager.shared
+    @Binding var mapState: MapViewState
+    @EnvironmentObject var locationViewModel: LocationSearchViewModel
+    
+    func makeUIView(context: Context) -> some UIView {
+        mapView.delegate = context.coordinator
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+         
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        switch mapState {
+        case .noInput:
+            context.coordinator.clearMapViewAndRecenterOnUserLocation()
+            break
+        case .searchingForLocation:
+            break
+        case .locationSelected:
+            if let coordinate = locationViewModel.selectedTaxiLocation?.coordinate {
+                context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
+                context.coordinator.configurePolyline(withDestinationCoordinate: coordinate)
+            }
+            break
+        case .polylineAdded:
+            break
+        }
+    }
+    
+    func makeCoordinator() -> MapCoordinator {
+        return MapCoordinator(parent: self)
+    }
+}
+
+extension TaxiMapViewRepresentable {
+    class MapCoordinator: NSObject, MKMapViewDelegate {
+        
+        // MARK: - Properties
+        let parent: TaxiMapViewRepresentable
+        var userLocationCoordinate: CLLocationCoordinate2D?
+        var currentRegion: MKCoordinateRegion?
+        
+        // MARK: - Lifecycle
+        init(parent: TaxiMapViewRepresentable) {
+            self.parent = parent
+            super.init()
+        }
+        
+        //MARK: - MKMapViewDelegate
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            self.userLocationCoordinate = userLocation.coordinate
+            
+            let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            )
+            
+            self.currentRegion = region
+            
+            parent.mapView.setRegion(region, animated: true)
+        }
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let polyline = MKPolylineRenderer(overlay: overlay)
+            polyline.strokeColor = .systemBlue
+            polyline.lineWidth = 6
+            return polyline
+        }
+        
+        // MARK: - Helpers
+        func addAndSelectAnnotation(withCoordinate coordinate: CLLocationCoordinate2D) {
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            
+            let anno = MKPointAnnotation()
+            anno.coordinate = coordinate
+            parent.mapView.addAnnotation(anno)
+            parent.mapView.selectAnnotation(anno, animated: true)
+            
+            //parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+        }
+                
+        func configurePolyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D) {
+            guard let userLocationCoordinate = self.userLocationCoordinate else { return }
+            parent.locationViewModel.getDestinationRoute(from: userLocationCoordinate, to: coordinate) { route in
+                self.parent.mapView.addOverlay(route.polyline)
+                self.parent.mapState = .polylineAdded
+                let rect = self.parent.mapView.mapRectThatFits(route.polyline.boundingMapRect, edgePadding: .init(top: 64, left: 32, bottom: 500, right: 32))
+                self.parent.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
+            }
+        }
+
+        
+        func clearMapViewAndRecenterOnUserLocation() {
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            parent.mapView.removeOverlays(parent.mapView.overlays)
+            
+            if let currentRegion = currentRegion {
+                parent.mapView.setRegion(currentRegion, animated: true)
+            }
+        }
+    }
+    
+}
---
+++ TaxiApp/Core/LocationSearch/Views/LocationSearchView.swift
@@ -0,0 +1,74 @@
+//
+//  LocationSearchView.swift
+//  TaxiApp
+//
+//  Created by Joe on 2024-07-23.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+    @State private var startLocationText = ""
+    @Binding var mapState: MapViewState
+    @EnvironmentObject var viewModel: LocationSearchViewModel
+    
+    var body: some View {
+        VStack{
+            // header view
+            HStack{
+                VStack{ 
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 6, height: 6)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 24)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 6, height: 6)
+                }
+                
+                VStack{
+                    TextField("Current Location", text: $startLocationText)
+                        .frame(height: 32)
+                        .background(Color(.systemBackground))
+                        .padding(.trailing)
+                    
+                    TextField("Where to?", text: $viewModel.queryFragment)
+                        .frame(height: 32)
+                        .background(Color(.systemGray4))
+                        .padding(.trailing)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 64)
+            
+            Divider()
+                .padding(.vertical)
+            
+            // list view
+            ScrollView{
+                VStack(alignment: .leading) {
+                    ForEach(viewModel.results, id: \.self) { result in
+                        LocationSearchResultCell(title: result.title, subtitle: result.subtitle)
+                            .onTapGesture {
+                                withAnimation(.spring()) {
+                                    viewModel.selectedLocation(result)
+                                    mapState = .locationSelected
+                                }
+                            }
+                    }
+                }
+            }
+            
+        }
+        .background(Color.theme.backgroundColor)
+        .background(.white)
+    }
+}
+
+#Preview {
+    LocationSearchView(mapState: .constant(.searchingForLocation))
+}
