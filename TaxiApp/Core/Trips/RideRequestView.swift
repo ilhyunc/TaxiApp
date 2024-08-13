02e2ac1a9bc87f58b450d73ec0a655335de06abf
Ilhyun Cho <icho8@myseneca.ca>
2024-08-13 09:43:55 -0400

    AUG-13

---
+++ TaxiApp/Core/Trips/RideRequestView.swift
@@ -0,0 +1,163 @@
+//
+//  RideRequestView.swift
+//  TaxiApp
+//
+//  Created by Joe on 2024-07-25.
+//
+
+import SwiftUI
+
+struct RideRequestView: View {
+    @State private var selectedRideType: RideType = .taxi
+    @EnvironmentObject var locationViewModel: LocationSearchViewModel
+    
+    var body: some View {
+        VStack {
+            Capsule()
+                .foregroundColor(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+                .padding(.top, 8)
+            
+            // trip info view
+            HStack{
+                // indicator view
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 8, height: 8)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 32)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 8, height: 8)
+                }
+                
+                
+                VStack (alignment: .leading, spacing: 24) {
+                    HStack {
+                        Text("Current location")
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(.gray)
+                        
+                        Spacer()
+                        
+                        Text(locationViewModel.pickUpTime ?? "")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.bottom, 10)
+                    
+                    HStack {
+                        if let location = locationViewModel.selectedTaxiLocation {
+                            Text(location.title)
+                                .font(.system(size: 16, weight: .semibold))
+                        }
+                        
+                        Spacer()
+                        
+                        Text(locationViewModel.dropOffTime ?? "")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(.leading, 8)
+            }
+            .padding()
+            
+            Divider()
+            
+            
+            // ride type selection view
+            Text("SUGGESTED RIDES")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .padding()
+                .foregroundColor(.gray)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 12) {
+                    ForEach(RideType.allCases) { type in
+                        VStack(alignment: .leading) {
+                            Image(type.imageName)
+                                .resizable()
+                                .scaledToFit()
+                            
+                            VStack (alignment: .leading, spacing: 4){
+                                Text(type.description)
+                                    .font(.system(size: 14, weight: .semibold))
+                                
+                                Text(locationViewModel.computeRidePrice(forType: type).toCurrency())
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            .padding()
+                            
+                            
+                        }
+                        .frame(width:  112, height: 140)
+                        .foregroundColor(type == selectedRideType ? .white : Color.theme.primaryTextColor)
+                        .background(type == selectedRideType ? .blue : Color.theme.secondaryBackgroundColor)
+                        .scaleEffect(type == selectedRideType ? 1.2 : 1.0)
+                        .cornerRadius(10)
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                selectedRideType = type
+                            }
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal)
+            
+            Divider()
+                .padding(.vertical, 8)
+            
+            // payment option view
+            HStack (spacing: 12) {
+                Text("Visa")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(6)
+                    .background(.blue)
+                    .cornerRadius(4)
+                    .foregroundColor(.white)
+                    .padding(.leading)
+                
+                Text("**** 1234")
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .imageScale(/*@START_MENU_TOKEN@*/.medium/*@END_MENU_TOKEN@*/)
+                    .padding()
+            }
+            .frame(height: 50)
+            .background(Color(Color.theme.backgroundColor))
+            .cornerRadius(10)
+            .padding(.horizontal)
+            
+            // request ride button
+            Button{
+                
+            } label:  {
+                Text("CONFIRM RIDE")
+                    .fontWeight(.bold)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
+            }
+        }
+        .padding(.bottom, 24)
+        .background(Color.theme.secondaryBackgroundColor)
+        .cornerRadius(16)
+    }
+}
+
+#Preview {
+    RideRequestView()
+}
---
+++ TaxiApp/Models/MapViewState.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewState.swift
+//  TaxiApp
+//
+//  Created by Joe on 2024-07-25.
+//
+
+import Foundation
+
+enum MapViewState {
+    case noInput
+    case searchingForLocation
+    case locationSelected
+    case polylineAdded
+}
