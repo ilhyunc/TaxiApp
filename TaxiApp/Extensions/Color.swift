5e3c9506995460a87a5a914302266b6cea0ac458
Ilhyun Cho <icho8@myseneca.ca>
2024-08-16 09:47:58 -0400

    AUG-16

---
+++ TaxiApp/Extensions/Color.swift
@@ -0,0 +1,18 @@
+//
+//  Color.swift
+//  TaxiApp
+//
+//  Created by Joe on 2024-08-01.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let backgroundColor = Color("BackgroundColor")
+    let secondaryBackgroundColor = Color("SecondaryBackgroundColor")
+    let primaryTextColor = Color("PrimaryTextColor")
+}
