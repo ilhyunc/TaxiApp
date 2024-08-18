318e55ce2fa6622d3333caf224d3438e989952aa
Ilhyun Cho <icho8@myseneca.ca>
2024-08-17 21:34:18 -0400

    AUG-17

---
+++ TaxiApp/Extensions/Double.swift
@@ -0,0 +1,23 @@
+//
+//  Double.swift
+//  TaxiApp
+//
+//  Created by Joe on 2024-08-01.
+//
+
+import Foundation
+
+extension Double {
+    private var currencyFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        
+        return formatter
+    }
+    
+    func toCurrency() -> String {
+        return currencyFormatter.string(for: self) ?? ""
+    }
+}
---
+++ TaxiApp/Models/TaxiLocation.swift
@@ -0,0 +1,13 @@
+//
+//  TaxiLocation.swift
+//  TaxiApp
+//
+//  Created by Joe on 2024-08-01.
+//
+
+import CoreLocation
+
+struct TaxiLocation {
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
