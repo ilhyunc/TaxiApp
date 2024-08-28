2412026e3a01f731f91968ca8e04a3f52f64a86e
Ilhyun Cho <icho8@myseneca.ca>
2024-08-28 09:44:33 -0400

    .

--- TaxiApp/Managrs/LocationManager.swift
+++ TaxiApp/Managrs/LocationManager.swift
@@ -7,7 +7,7 @@
 
 import CoreLocation
 
-class LocationManager: NSObject, ObservableObject{
+class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
     static let shared = LocationManager()
     @Published var userLocation: CLLocationCoordinate2D?
@@ -21,7 +21,7 @@ class LocationManager: NSObject, ObservableObject{
     }
 }
 
-extension LocationManager: CLLocationManagerDelegate{
+extension LocationManager: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.first else {return}
         self.userLocation = location.coordinate
